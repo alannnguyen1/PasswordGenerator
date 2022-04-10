69b8b6601eba25d0f3bde9e0a885024cf285810f
Alan Nguyen <nguyenalan2000@gmail.com>
2022-04-09 17:13:24 -0700

    removed for loop in password gen

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -37,8 +37,6 @@ public class PasswordGenerator {
         System.out.println(password);
     }
     public static void main(String[] args) {
-        for (int i = 0 ; i < 10; i++) {
-            generate();
-        }
+        generate();
     }
 }
