be4106abc50091bdf512005769e3f741f24beee1
Alan Nguyen <nguyenalan2000@gmail.com>
2022-05-02 19:22:18 -0700

    Changed password generator to take input

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -3,40 +3,54 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class PasswordGenerator {
-    public static void generate(){
+    public static String generate(boolean letters, boolean symbols, boolean numbers, boolean capitals, int passLength){
+        if(!letters && !symbols && !numbers && !capitals) {
+            return "Please select at least one requirement";
+        }
+        if(passLength == 0) {
+            return "Please insert a length greater than 0";
+        }
         Random rand = new Random();
         ArrayList<String> lettersList = new ArrayList<>(Arrays.asList("a","b","c","d","e","f","g","h","i","j","k","l","m",
                 "n","o","p","q","r","s","t","u","v","w","x","y","z"));
-        ArrayList<String> specialList = new ArrayList<>(Arrays.asList("!","@","#","$","%","^","&","*","(",")"));
+        ArrayList<String> symbolsList = new ArrayList<>(Arrays.asList("!","@","#","$","%","^","&","*","(",")"));
         ArrayList<String> numbersList = new ArrayList<>(Arrays.asList("1","2","3","4","5","6","7","8","9","0"));
-        int length = 8 + rand.nextInt(8);
-        int capital = rand.nextInt(length);
-        int special = rand.nextInt(length);
+        int capital = rand.nextInt(passLength);
+        int special = rand.nextInt(passLength);
         while (capital == special) {
-            special = rand.nextInt(length);
+            special = rand.nextInt(passLength);
         }
-        int number = rand.nextInt(length);
+        int number = rand.nextInt(passLength);
         while (capital == number || special == number) {
-            number = rand.nextInt(length);
+            number = rand.nextInt(passLength);
         }
         StringBuilder password = new StringBuilder("");
-        for(int i = 0; i < length; i++) {
-            if(i == capital){
-                password.append(lettersList.get(rand.nextInt(lettersList.size())).toUpperCase());
-            }
-            else if(i == special){
-                password.append(specialList.get(rand.nextInt(specialList.size())));
-            }
-            else if(i == number){
-                password.append(numbersList.get(rand.nextInt(numbersList.size())));
-            }
-            else{
-                password.append(lettersList.get(rand.nextInt(lettersList.size())));
-            }
+        for(int i = 0; i < passLength; i++) {
+            boolean done = false;
+            do {
+                int picker = rand.nextInt(4);
+                if(picker == 0 && capitals){
+                    password.append(lettersList.get(rand.nextInt(lettersList.size())).toUpperCase());
+                    done = true;
+                }
+                else if(picker == 1 && symbols){
+                    password.append(symbolsList.get(rand.nextInt(symbolsList.size())));
+                    done = true;
+                }
+                else if(picker == 2 && numbers){
+                    password.append(numbersList.get(rand.nextInt(numbersList.size())));
+                    done = true;
+                }
+                else if (picker == 3 && letters){
+                    password.append(lettersList.get(rand.nextInt(lettersList.size())));
+                    done = true;
+                }
+            } while (!done);
         }
-        System.out.println(password);
+        return password.toString();
     }
     public static void main(String[] args) {
-        generate();
+        String pass = generate(true, true, true, true, 5);
+        System.out.println(pass);
     }
 }
